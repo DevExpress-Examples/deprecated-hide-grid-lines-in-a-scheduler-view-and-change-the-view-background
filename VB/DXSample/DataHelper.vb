821585f38bf6dee53141f67ed6f9158b327360c6
DevExpressExampleBot <fakeEmail>
2022-12-17 04:53:48 +0400

    Source auto update [skip ci]

--- VB/DXSample/DataHelper.vb
+++ VB/DXSample/DataHelper.vb
@@ -1,47 +1,17 @@
-﻿Imports System
 Imports System.Collections.ObjectModel
 
 Namespace DXSample.ViewModels
-	Friend Class DataHelper
-		Public Shared Function GetAppointments() As ObservableCollection(Of AppointmentViewModel)
-			Dim collection As New ObservableCollection(Of AppointmentViewModel)() From {
-				New AppointmentViewModel() With {
-					.Id = 1,
-					.StartTime = DateTime.Today.AddDays(-1),
-					.EndTime = DateTime.Today.AddDays(4),
-					.ResourceId = 1,
-					.Subject = "Task 1"
-				},
-				New AppointmentViewModel() With {
-					.Id = 2,
-					.StartTime = DateTime.Today,
-					.EndTime = DateTime.Today.AddDays(5),
-					.ResourceId = 1,
-					.Subject = "Task 2"
-				},
-				New AppointmentViewModel() With {
-					.Id = 3,
-					.StartTime = DateTime.Today.AddDays(2),
-					.EndTime = DateTime.Today.AddDays(7),
-					.ResourceId = 2,
-					.Subject = "Task 3"
-				}
-			}
-			Return collection
-		End Function
 
-		Public Shared Function GetResources() As ObservableCollection(Of ResourceViewModel)
-			Dim collection As New ObservableCollection(Of ResourceViewModel)() From {
-				New ResourceViewModel() With {
-					.Id = 1,
-					.Caption = "Work"
-				},
-				New ResourceViewModel() With {
-					.Id = 2,
-					.Caption = "Personal"
-				}
-			}
-			Return collection
-		End Function
-	End Class
+    Friend Class DataHelper
+
+        Public Shared Function GetAppointments() As ObservableCollection(Of AppointmentViewModel)
+            Dim collection As ObservableCollection(Of AppointmentViewModel) = New ObservableCollection(Of AppointmentViewModel)() From {New AppointmentViewModel() With {.Id = 1, .StartTime = Date.Today.AddDays(-1), .EndTime = Date.Today.AddDays(4), .ResourceId = 1, .Subject = "Task 1"}, New AppointmentViewModel() With {.Id = 2, .StartTime = Date.Today, .EndTime = Date.Today.AddDays(5), .ResourceId = 1, .Subject = "Task 2"}, New AppointmentViewModel() With {.Id = 3, .StartTime = Date.Today.AddDays(2), .EndTime = Date.Today.AddDays(7), .ResourceId = 2, .Subject = "Task 3"}}
+            Return collection
+        End Function
+
+        Public Shared Function GetResources() As ObservableCollection(Of ResourceViewModel)
+            Dim collection As ObservableCollection(Of ResourceViewModel) = New ObservableCollection(Of ResourceViewModel)() From {New ResourceViewModel() With {.Id = 1, .Caption = "Work"}, New ResourceViewModel() With {.Id = 2, .Caption = "Personal"}}
+            Return collection
+        End Function
+    End Class
 End Namespace
--- VB/DXSample/SchedulingViewModel.vb
+++ VB/DXSample/SchedulingViewModel.vb
@@ -1,94 +1,112 @@
-﻿Imports System
 Imports System.Collections.ObjectModel
 Imports DevExpress.Mvvm
 
 Namespace DXSample.ViewModels
-	Public Class SchedulingViewModel
-		Inherits ViewModelBase
 
-		Public Sub New()
-			Appointments = DataHelper.GetAppointments()
-			Resources = DataHelper.GetResources()
-		End Sub
-		Public Property Appointments() As ObservableCollection(Of AppointmentViewModel)
-			Get
-				Return GetValue(Of ObservableCollection(Of AppointmentViewModel))()
-			End Get
-			Set(ByVal value As ObservableCollection(Of AppointmentViewModel))
-				SetValue(value)
-			End Set
-		End Property
-		Public Property Resources() As ObservableCollection(Of ResourceViewModel)
-			Get
-				Return GetValue(Of ObservableCollection(Of ResourceViewModel))()
-			End Get
-			Set(ByVal value As ObservableCollection(Of ResourceViewModel))
-				SetValue(value)
-			End Set
-		End Property
-	End Class
-	Public Class AppointmentViewModel
-		Inherits ViewModelBase
+    Public Class SchedulingViewModel
+        Inherits ViewModelBase
 
-		Public Property Id() As Integer
-			Get
-				Return GetValue(Of Integer)()
-			End Get
-			Set(ByVal value As Integer)
-				SetValue(value)
-			End Set
-		End Property
-		Public Property StartTime() As DateTime
-			Get
-				Return GetValue(Of DateTime)()
-			End Get
-			Set(ByVal value As DateTime)
-				SetValue(value)
-			End Set
-		End Property
-		Public Property EndTime() As DateTime
-			Get
-				Return GetValue(Of DateTime)()
-			End Get
-			Set(ByVal value As DateTime)
-				SetValue(value)
-			End Set
-		End Property
-		Public Property Subject() As String
-			Get
-				Return GetValue(Of String)()
-			End Get
-			Set(ByVal value As String)
-				SetValue(value)
-			End Set
-		End Property
-		Public Property ResourceId() As Integer
-			Get
-				Return GetValue(Of Integer)()
-			End Get
-			Set(ByVal value As Integer)
-				SetValue(value)
-			End Set
-		End Property
-	End Class
-	Public Class ResourceViewModel
-		Inherits ViewModelBase
+        Public Sub New()
+            Appointments = DataHelper.GetAppointments()
+            Resources = DataHelper.GetResources()
+        End Sub
 
-		Public Property Id() As Integer
-			Get
-				Return GetValue(Of Integer)()
-			End Get
-			Set(ByVal value As Integer)
-				SetValue(value)
-			End Set
-		End Property
-		Public Property Caption() As String
-			Get
-				Return GetValue(Of String)()
-			End Get
-			Set(ByVal value As String)
-				SetValue(value)
-			End Set
-		End Property
-	End Class
+        Public Property Appointments As ObservableCollection(Of AppointmentViewModel)
+            Get
+                Return GetValue(Of ObservableCollection(Of AppointmentViewModel))()
+            End Get
+
+            Set(ByVal value As ObservableCollection(Of AppointmentViewModel))
+                SetValue(value)
+            End Set
+        End Property
+
+        Public Property Resources As ObservableCollection(Of ResourceViewModel)
+            Get
+                Return GetValue(Of ObservableCollection(Of ResourceViewModel))()
+            End Get
+
+            Set(ByVal value As ObservableCollection(Of ResourceViewModel))
+                SetValue(value)
+            End Set
+        End Property
+    End Class
+
+    Public Class AppointmentViewModel
+        Inherits ViewModelBase
+
+        Public Property Id As Integer
+            Get
+                Return GetValue(Of Integer)()
+            End Get
+
+            Set(ByVal value As Integer)
+                SetValue(value)
+            End Set
+        End Property
+
+        Public Property StartTime As Date
+            Get
+                Return GetValue(Of Date)()
+            End Get
+
+            Set(ByVal value As Date)
+                SetValue(value)
+            End Set
+        End Property
+
+        Public Property EndTime As Date
+            Get
+                Return GetValue(Of Date)()
+            End Get
+
+            Set(ByVal value As Date)
+                SetValue(value)
+            End Set
+        End Property
+
+        Public Property Subject As String
+            Get
+                Return GetValue(Of String)()
+            End Get
+
+            Set(ByVal value As String)
+                SetValue(value)
+            End Set
+        End Property
+
+        Public Property ResourceId As Integer
+            Get
+                Return GetValue(Of Integer)()
+            End Get
+
+            Set(ByVal value As Integer)
+                SetValue(value)
+            End Set
+        End Property
+    End Class
+
+    Public Class ResourceViewModel
+        Inherits ViewModelBase
+
+        Public Property Id As Integer
+            Get
+                Return GetValue(Of Integer)()
+            End Get
+
+            Set(ByVal value As Integer)
+                SetValue(value)
+            End Set
+        End Property
+
+        Public Property Caption As String
+            Get
+                Return GetValue(Of String)()
+            End Get
+
+            Set(ByVal value As String)
+                SetValue(value)
+            End Set
+        End Property
+    End Class
 End Namespace
